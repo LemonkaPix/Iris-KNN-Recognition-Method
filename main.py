c4cd4e709f6123b86c5fa1bb6b0cfd5a00350c09
LemonkaPix <maciejwrobel029@gmail.com>
2025-01-13 14:17:35 +0100

    Model test and user service

--- main.py
+++ main.py
@@ -7,28 +7,59 @@ dataset.pop(len(dataset)-1)
 vectors = []
 for item in dataset:
     temp = item.split(",")
-    vectors.append({
-        "sepal_length": float(temp[0]),
-        "sepal_width": float(temp[1]),
-        "petal_length": float(temp[2]),
-        "petal_width": float(temp[3]),
-        "species": temp[4]
-    })
+    vectors.append([
+        float(temp[0]),
+        float(temp[1]),
+        float(temp[2]),
+        float(temp[3]),
+        temp[4]
+    ])
 
 trainData = []
 testData = []
-testDataCount = int(len(vectors) * 0.2)
 for i in range(len(vectors)):
-    if i < len(vectors) - testDataCount:
+    if i % 5 != 0:
         trainData.append(vectors[i])
     else:
         testData.append(vectors[i])
 
-def distance_4d(vector1, vector2):
+def Distance_4d(vector1, vector2):
     return math.sqrt(sum((v1 - v2) ** 2 for v1, v2 in zip(vector1, vector2)))
 
+def RecognizeIris(input, data):
+
+    closestNeighbourDistance = math.inf
+    closestNeighbourString = ""
+
+    for record in data:
+        distance = Distance_4d(input[0:4], record[0:4])
+        if distance < closestNeighbourDistance:
+            closestNeighbourDistance = distance
+            closestNeighbourString = record[-1]
+    return closestNeighbourString
+
+
 correctPredictionsCount = 0
-closestNeighbourDistance = 100
-closestNeighbourString = ""
-for i in testData:
-    
+for case in testData:
+
+    answer = case[-1]
+
+    prediction = RecognizeIris(case, trainData)
+
+    if prediction == answer:
+        correctPredictionsCount += 1
+
+percentage = float(correctPredictionsCount) / float(len(testData)) * 100.0
+print("Model precission: " + str(percentage) + "%")
+print()
+
+print("Sepal length (cm): ")
+userInput = [float(input())]
+print("Sepal width (cm): ")
+userInput.append(float(input()))
+print("Petal length (cm): ")
+userInput.append(float(input()))
+print("Petal width (cm): ")
+userInput.append(float(input()))
+
+print("Your iris is " + RecognizeIris(userInput, vectors))
